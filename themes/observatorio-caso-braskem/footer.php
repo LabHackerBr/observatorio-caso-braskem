04bb320c73f1ab180258e1a121e24df7c687dfb0
Julia <julia.maria@hacklab.com.br>
2025-04-11 16:44:50 -0300

    Style footer desktop

--- themes/observatorio-caso-braskem/footer.php
+++ themes/observatorio-caso-braskem/footer.php
@@ -5,6 +5,17 @@
         <div class="main-footer__widgets container">
             <?php dynamic_sidebar('footer_widgets') ?>
         </div>
+        <div class="main-footer__legal">
+            <div class="main-footer__legal-row">
+                <span>
+                    <a class="termos-uso" href="<?php get_site_url()?>/politica-de-privacidade" target="_blank"><span><?php _e('Terms of use', 'hacklabr') ?></span></a>
+                    <p>e</p>
+                    <a class="privacidade" href="<?php get_site_url()?>/politica-de-privacidade" target="_blank"><span><?php _e('Privacy Policy', 'hacklabr') ?></span></a>
+                </span>
+
+                <?php get_template_part( 'template-parts/site-by-hacklab' ); ?>
+            </div>
+        </div>
     </footer>
 <?php endif; ?>
 <?php wp_footer() ?>
