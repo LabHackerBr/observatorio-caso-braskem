794dc44e1310ff17e3d99e4676e3b6df8e5ecee8
Julia <julia.maria@hacklab.com.br>
2025-05-07 17:00:19 -0300

    Adjust zip script to exclude node_modules from blocks v2

--- dev-scripts/zip.sh
+++ dev-scripts/zip.sh
@@ -3,4 +3,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CDIR=$( pwd )
 cd $DIR/../themes
 rm -f ../zips/observatorio-caso-braskem.zip
-zip -r ../zips/observatorio-caso-braskem.zip observatorio-caso-braskem -x "observatorio-caso-braskem/node_modules/*"
+zip -r ../zips/observatorio-caso-braskem.zip observatorio-caso-braskem -x "observatorio-caso-braskem/node_modules/*" -x "observatorio-caso-braskem/library/blocks/v2/node_modules/*"
