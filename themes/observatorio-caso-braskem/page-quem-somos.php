b1de08598d6af7754c76dc0ca4bf22177d75e054
Carolina Pedrosa <carolinasouza0@gmail.com>
2025-05-09 14:35:41 -0300

    Adjust who we are page container size

--- themes/observatorio-caso-braskem/page-quem-somos.php
+++ themes/observatorio-caso-braskem/page-quem-somos.php
@@ -2,7 +2,7 @@
 get_header();
 ?>
 <div class="who-we-are has-primary-dark-background-color">
-    <div class="container">
+    <div class="container container--wide">
         <div class="post-header post-header__separator">
             <h1 class="post-header__title">
                 <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/exclamation-point.png" alt="exclamation-point" class="exclamation-icon">
