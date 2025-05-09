8ec1ab3e8d6c19dbe02faf007c979c3de045ac6d
Carolina Pedrosa <carolinasouza0@gmail.com>
2025-05-09 14:15:51 -0300

    Add who we are page

---
+++ themes/observatorio-caso-braskem/page-quem-somos.php
@@ -0,0 +1,17 @@
+<?php
+get_header();
+?>
+<div class="who-we-are has-primary-dark-background-color">
+    <div class="container">
+        <div class="post-header post-header__separator">
+            <h1 class="post-header__title">
+                <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/exclamation-point.png" alt="exclamation-point" class="exclamation-icon">
+                <?php the_title() ?>
+            </h1>
+        </div>
+        <div class="post-content content content--normal">
+            <?php the_content() ?>
+        </div>
+    </div>
+</div>
+<?php get_footer();
