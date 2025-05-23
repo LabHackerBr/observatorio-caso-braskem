63d61705f33a3d4bbc5a05382b892d9bdea84008
Carolina Pedrosa <carolinasouza0@gmail.com>
2025-05-23 14:31:34 -0300

    Adjust archive-header template

--- themes/observatorio-caso-braskem/template-parts/title/archive-header.php
+++ themes/observatorio-caso-braskem/template-parts/title/archive-header.php
@@ -4,36 +4,62 @@
  * Mount the title
  */
 $title = '';
-$search_query = get_search_query( false );
 
-if ( is_category() ) {
+if ( is_search() ) {
+    $count = (int) $wp_query->found_posts;
+    $results_text = ($count === 1) ? __('resultado', 'hacklabr') : __('resultados', 'hacklabr');
+
+    $title = sprintf(
+        '%s <span class="search-page-results-count">(%d %s)</span>',
+        __('RESULTADO DE BUSCA', 'hacklabr'),
+        $count,
+        $results_text
+    );
+
+} elseif ( is_category() ) {
     $title = single_cat_title( '', false );
 } else {
     $title = get_the_title();
-};
+}
 
-if ( ! empty( $search_query ) ) {
-    $title = 'Você pesquisou por: <span class="highlighted">' . esc_attr( $search_query  ) . '</span>';
-} else {
-    $title = 'Pesquisar';
-};?>
+
+?>
 
 <header class="archive-header__content">
-    <div class="archive-header__title">
-        <h1>
-            <?php echo apply_filters( 'the_title' , $title ); ?>
-        </h1>
-        <?php
-        if ( is_search() ) :
-            get_search_form();
-        endif; ?>
-    </div>
+    <div class="archive-header__background"></div>
 
-    <div class="archive-header__excerpt">
-        <?php the_archive_description(); ?>
-    </div>
+    <div class="archive-header__content-overlay">
+        <div class="archive-header__title-container">
+            <div class="archive-header__title">
+                <h1>
+                    <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/exclamation-point.png" alt="exclamation-point" class="exclamation-icon">
+                    <?php echo apply_filters( 'the_title' , $title ); ?>
+                </h1>
 
-    <div class="archive-header__results">
-        <p><span><?= $wp_query->found_posts;?></span><?php _e(' Results Found', 'hacklabr');?></p>
+                <?php if ( is_search() ) : ?>
+                    <div class="search-form-wrapper-on-archive">
+                        <?php
+                        get_search_form();
+                        ?>
+                        <button type="submit" class="search-submit-icon" form="searchform">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/search-icon-black.svg" alt="<?php esc_attr_e( 'Pesquisar', 'hacklabr' ); ?>">
+                        </button>
+                    </div>
+                <?php endif; ?>
+
+            </div>
+
+            <?php if ( is_search() && $wp_query->found_posts > 0 ) : ?>
+                <?php get_template_part( 'template-parts/search-filters' ); ?>
+            <?php endif; ?>
+        </div>
+
+        <div class="archive-header__excerpt">
+            <?php the_archive_description(); ?>
+        </div>
+
+        <!-- <div class="archive-header__results">
+            <p><span><?= $wp_query->found_posts;?></span><?php _e(' Results Found', 'hacklabr');?></p>
+        </div> -->
     </div>
 </header><!-- /.c-title.title-default -->
