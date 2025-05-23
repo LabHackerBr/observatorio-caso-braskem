6a47bec84a8e0229e78447acdf28a95309550aa2
Carolina Pedrosa <carolinasouza0@gmail.com>
2025-05-23 14:33:41 -0300

    Adjust search page

--- themes/observatorio-caso-braskem/search.php
+++ themes/observatorio-caso-braskem/search.php
@@ -4,29 +4,31 @@ get_header();
 
 
 <div class="container container--wide">
-    <?php get_template_part( 'template-parts/title/archive-header' ); ?>
+    <div class="search-container">
+        <?php get_template_part( 'template-parts/title/archive-header' ); ?>
 
-    <main class="posts-grid__content">
+        <main class="posts-grid__content">
+            <?php
+            if ( have_posts() ) {
+                while ( have_posts() ) : the_post();
+                    get_template_part( 'template-parts/post-card', 'horizontal' );
+                endwhile;
+            } else {
+                get_template_part( 'template-parts/content/no-post' );
+            }; ?>
+        </main>
+
         <?php
-        if ( have_posts() ) {
-            while ( have_posts() ) : the_post();
-                get_template_part( 'template-parts/post-card', 'vertical' );
-            endwhile;
-        } else {
-            get_template_part( 'template-parts/content/no-post' );
-        }; ?>
-    </main>
+        the_posts_pagination([
+            'prev_text' => __( '<iconify-icon icon="iconamoon:arrow-left-2-bold"></iconify-icon>', 'hacklbr'),
+            'next_text' => __( '<iconify-icon icon="iconamoon:arrow-right-2-bold"></iconify-icon>', 'hacklbr'),
 
-    <?php
-    the_posts_pagination([
-        'prev_text' => __( '<iconify-icon icon="iconamoon:arrow-left-2-bold"></iconify-icon>', 'hacklbr'),
-        'next_text' => __( '<iconify-icon icon="iconamoon:arrow-right-2-bold"></iconify-icon>', 'hacklbr'),
+        ]); ?>
 
-    ]); ?>
-
-    <aside class="">
-        <?php dynamic_sidebar( 'sidebar-search' ) ?>
-    </aside>
+        <aside class="">
+            <?php dynamic_sidebar( 'sidebar-search' ) ?>
+        </aside>
+    </div>
 </div><!-- /.container -->
 
 
