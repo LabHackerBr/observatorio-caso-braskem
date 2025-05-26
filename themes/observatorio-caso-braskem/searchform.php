fa96c1f6f37e8015bb435d76b73d86a2259235b6
Carolina Pedrosa <carolinasouza0@gmail.com>
2025-05-26 10:59:12 -0300

    Add searchform template

---
+++ themes/observatorio-caso-braskem/searchform.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Template para exibir o formulário de busca personalizado.
+ */
+
+$unique_input_id = 'search-field-' . esc_attr( uniqid() );
+
+$form_id = 'main-search-page-form';
+?>
+<form role="search" method="get" id="<?php echo esc_attr( $form_id ); ?>" class="search-form"  action="<?php echo esc_url( home_url( '/' ) ); ?>">
+    <label for="<?php echo $unique_input_id; ?>" class="screen-reader-text"><?php esc_html_e( 'Pesquisar por:', 'hacklabr' ); ?></label>
+    <input
+        type="search"
+        id="<?php echo $unique_input_id; ?>"
+        class="search-field"
+        placeholder="<?php esc_attr_e( 'Termo buscado...', 'hacklabr' ); ?>"
+        value="<?php echo get_search_query(); ?>"
+        name="s"
+    />
+</form>
