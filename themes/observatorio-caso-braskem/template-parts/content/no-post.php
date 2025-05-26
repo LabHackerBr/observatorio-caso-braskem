e702308ed581e96dc17060d9bedf477d704adff8
Carolina Pedrosa <carolinasouza0@gmail.com>
2025-05-26 15:05:37 -0300

    Adjust no-post template

--- themes/observatorio-caso-braskem/template-parts/content/no-post.php
+++ themes/observatorio-caso-braskem/template-parts/content/no-post.php
@@ -1,5 +1,22 @@
 <div class="search__alert no-post">
-    <h2 class="search__title">
-        <?php _e( 'Sorry, but nothing matched your search criteria. Please try again with some different keywords.', 'hacklabr'); ?>
-    </h2>
+    <div class="no-post__wrapper">
+        <h2>
+            <?php _e( 'Nada encontrado', 'hacklabr'); ?>
+        </h2>
+
+        <div class="no-post__content">
+            <p>
+                <?php _e( 'Desculpe, nada corresponde aos seus termos de pesquisa.', 'hacklabr'); ?>
+            </p>
+            <p>
+                <?php _e( 'Tente novamente com algumas palavras-chave diferentes.', 'hacklabr'); ?>
+            </p>
+        </div>
+
+        <div class="no-post__button">
+            <a href="<?php echo home_url(); ?>" class="button">
+                <?php _e( 'Voltar para home', 'hacklabr'); ?>
+            </a>
+        </div>
+    </div>
 </div>
