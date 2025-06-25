977b0134e1cbbfb9563cd3a1136a748e2066a73c
Thiago Monteiro <thiagomonteiro.geo@gmail.com>
2025-06-25 15:41:06 -0300

    retira mapas culturais estilo bloco instagram

--- themes/observatorio-caso-braskem/library/blocks/v2/src/events-from-mapas-culturais/includes/helpers.php
+++ themes/observatorio-caso-braskem/library/blocks/v2/src/events-from-mapas-culturais/includes/helpers.php
@@ -2,54 +2,6 @@
 
 namespace hacklabr\v2;
 
-function build_mapas_culturais_api_url( $api_url, $from = null, $to = null, $event_select = null, $space_select = null, $limit = null, $page = null ) {
-    $query_params = [];
-
-    if ( $from ) {
-        $query_params[] = '%40from=' . sanitize_encode( $from );
-    }
-
-    if ( $to ) {
-        $query_params[] = '%40to=' . sanitize_encode( $to );
-    }
-
-    $query_params[] = 'event%3A%40select=' . sanitize_encode( $event_select ? $event_select : 'id,name,subTitle,files.avatar,seals,terms,classificacaoEtaria,singleUrl' );
-    $query_params[] = 'space%3A%40select=' . sanitize_encode( $space_select ? $space_select : 'id,name,endereco,files.avatar,singleUrl' );
-    $query_params[] = '%40limit=' . ( isset( $limit ) ? intval( $limit ) : 100 );
-    $query_params[] = '%40page=' . ( isset( $page ) ? intval( $page ) : 1 );
-
-    return esc_url( $api_url ) . '?' . implode( '&', $query_params );
-}
-
-function get_events_from_mapas_culturais( $from, $to, $limit ) {
-    $base_url = get_option( 'mapas_culturais_url', false );
-
-    if ( ! $base_url) {
-        return;
-    }
-
-    $base_url = rtrim( $base_url, '/' ) . '/';
-    $api_url = $base_url . 'api/event/occurrences';
-
-    $url = build_mapas_culturais_api_url( $api_url, $from, $to, null, null, $limit );
-
-    $response = wp_remote_get( $url );
-
-    if ( is_wp_error( $response ) ) {
-        do_action( 'logger', [
-            'line'    => __LINE__,
-            'file'    => __FILE__,
-            'message' => 'Erro ao buscar eventos do Mapas Culturais: ' . $response->get_error_message(),
-        ] );
-    } else {
-        $body = wp_remote_retrieve_body( $response );
-        $data = json_decode( $body, true );
-        return $data;
-    }
-
-    return false;
-}
-
 function render_event( $event ) {
     $html = '';
 
